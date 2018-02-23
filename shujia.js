979f08133a72272c6b7887799f774a0bae326bc0
lyzaibupoaihua <36227036+lyzaibupoaihua@users.noreply.github.com>
2018-02-23 16:02:52 +0800

    增加工程

---
+++ shujia.js
@@ -0,0 +1,12 @@
+	// var zhanghao ='lyz'
+	// var huaxian=function(number){
+	// 	for(var i=0; i <= number; i++){
+	// 		var a="<hr>";
+	// 		 //class ="vertical-line id ="lenght-${i}"
+	// 		document.getElementById("yidu").innerHTML += a
+	// 		// var elem= getElementsById("lenght-${i}")
+	// 		// var c= (500-number)/500
+	// 		// elem.style.height = c
+	// 	}
+	// }
+	//  huaxian(30)
