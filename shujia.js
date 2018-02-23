8b5ade36146f77f644a8afcf4d39ead89a914281
lyzaibupoaihua <36227036+lyzaibupoaihua@users.noreply.github.com>
2018-02-23 22:21:42 +0800

    增加了一个鼠标点击的判断

--- shujia.js
+++ shujia.js
@@ -10,3 +10,39 @@
 	// 	}
 	// }
 	//  huaxian(30)
+	var log =function(){
+		
+			console.log.apply(console,arguments)
+	
+		
+	}
+	//判断鼠标点击的区域 已控制左右翻页
+	function whichElement(e){
+		var e = window.event
+		// if (!e) var e = window.event
+		// if (e.target) targ = e.target
+		// else if (e.srcElement) targ = e.srcElement
+		// log("dianjile",window.event)
+	// 	log("点击事件",e)
+		var clickedwidth =e.x
+		var sreenwidth = document.body.clientWidth
+		 if(clickedwidth * 2 >= sreenwidth ){
+		 	log("你点击的区域是右半边")
+
+		 }else{
+		 	log("你点击的区域是左半边")
+		 }
+		 	
+		// log("点击的宽度",clickedwidth)
+		// log("当前网页宽度",document.body.clientWidth)
+		// log("当前网页",document.body.clientHeight)
+		
+		// if (targ.nodeType == 3) // defeat Safari bug
+		// 	 log("0.2",targ)
+		//    targ = targ.parentNode
+
+		// var tname
+		// tname=targ.tagName
+		// // alert("You clicked on a " + tname + " element.")
+		// log("你点击的区域是",tname)
+	}
